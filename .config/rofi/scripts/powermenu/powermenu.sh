5dfa49eb72b85c621eba3cfaf9cebf41c6c1603f
wasifekbal <wasifekbal27@gmail.com>
2023-06-05 11:46:03 +0530

    minor icon changes.

--- .config/rofi/scripts/powermenu/powermenu.sh
+++ .config/rofi/scripts/powermenu/powermenu.sh
@@ -4,22 +4,27 @@
 poweroff=' Poweroff';
 reboot=' Reboot';
 lock=' Lock';
-suspend=' Suspend';
-logout=' Logout';
+suspend='󰒲 Suspend';
+logout='󰍃 Logout';
 
 
-# ROFI_THEME="~/.config/rofi/power.rasi"
 ROFI_THEME="~/.config/rofi/scripts/powermenu/power.rasi"
 
 uptime=$(uptime -p | cut -b 4- -)
-options="$poweroff\n$reboot\n$lock\n$suspend\n$logout" 
+options="$poweroff\n$reboot\n$lock\n$suspend\n$logout"
 
 choice=$(echo -e $options | rofi -dmenu -p "  " -mesg "Uptime: $uptime" -i -theme $ROFI_THEME);
 
 case "$choice" in
   $poweroff) poweroff ;;
   $reboot) reboot ;;
-  $lock) xdg-screensaver lock ;;
+  $lock)
+      if which "screenlock" >/dev/null 2>&1 && which "betterlockscreen" >/dev/null 2>&1; then
+          screenlock;
+      else
+          xdg-screensaver lock
+      fi
+  ;;
   $suspend) systemctl suspend ;;
   $logout) pkill -KILL -u $(whoami);;
   *) exit 1 ;;
