08e55f6548cf722fb597499497d31b4c0664b105
wasifekbal <wasifekbal27@gmail.com>
2024-01-13 21:52:06 +0530

    added apt-install.sh

---
+++ apt-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install -y jq zsh curl htop dconf-editor gnome-tweaks zsh ranger git sshfs tmux neofetch python3-venv python-is-python3 python3-pip dconf-editor sxhkd chrome-gnome-shell gnome-shell-extension-manager xdotool nomacs papirus-icon-theme arch-install-scripts resolvconf
+
+# for rofi and neovim
+sudo apt install -y curl build-essential cmake ninja-build meson gettext cmake unzip pango1.0-tools libpango1.0-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-randr0-dev libxcb-xinerama0-dev libstartup-notification0-dev bison
